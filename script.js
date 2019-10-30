4512a83497f320eb897bf0e44418564c6a8fe522
BenjaminElizondo <34512679+BenjaminElizondo@users.noreply.github.com>
2019-10-30 10:29:32 -0400

    STEP 11

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-    $('button').click(function(){
-        $('body').addClass('black');
+$(document).ready(function () {
+    $('body').click(function () {
+        if ($('body').hasClass('black')) {
+            $('body').removeClass('black').addClass('white');
+        }
+        else {
+            $('body').removeClass('white').addClass('black');
+        }
     });
 });
